ca079227533a79ed90aebb175b3484cc500f1a08
Ridwan Afwan K. F <ridwanakf@users.noreply.github.com>
2022-03-12 14:01:18 +0700

    Feature :: Setup Heroku (#1)
    * setup heroku
    
    * test fix deployment
    
    * downgrade some library version to support 1.13 (latest by heroku)
    
    * downgrade some library version to support 1.13 (latest by heroku)
    
    * downgrade some library version to support 1.13 (latest by heroku)

--- internal/app/config/server.go
+++ internal/app/config/server.go
@@ -1,7 +1,7 @@
 package config
 
 type Server struct {
-	Port                string `env:"SERVER_PORT"`
+	Port                string `env:"PORT"`
 	Debug               bool   `env:"SERVER_DEBUG"`
 	ReadTimeoutSeconds  int    `env:"SERVER_READ_TIMEOUT"`
 	WriteTimeoutSeconds int    `env:"SERVER_WRITE_TIMEOUT"`
